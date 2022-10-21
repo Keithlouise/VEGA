56228d30b52e0f9bcc9472698ce34adb0bb1c822
Keithlouise <116321053+Keithlouise@users.noreply.github.com>
2022-10-21 17:32:56 +0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace OrganizationProfile
+{
+    public partial class FrmRegistration : Form
+    {
+        private string _FullName;
+        private int _Age;
+        private long _ContactNo;
+        private long _StudentNo;
+
+        public long StudentNumber(string studNum)
+        {
+
+            _StudentNo = long.Parse(studNum);
+
+            return _StudentNo;
+        }
+
+        public long ContactNo(string Contact)
+        {
+            try
+            {
+
+                if (Regex.IsMatch(Contact, @"^[0-9]{10,11}$"))
+                {
+                    _ContactNo = long.Parse(Contact);
+                }
+                else
+                    MessageBox.Show("error!!!");
+            }
+
+            catch (FormatException e)
+            {
+                Console.WriteLine("error: "  + e.Message);
+            } 
+            finally
+            {
+                Console.WriteLine("Error");
+            }
+            return _ContactNo;
+        }
+
+        public string FullName(string LastName, string FirstName, string MiddleInitial)
+        {
+            if (Regex.IsMatch(LastName, @"^[a-zA-Z]+$") || Regex.IsMatch(FirstName, @"^[a-zA-Z]+$") || Regex.IsMatch(MiddleInitial, @"^[a-zA-Z]+$"))
+            {
+                _FullName = LastName + ", " + FirstName + ", " + MiddleInitial;
+            }
+
+            return _FullName;
+        }
+
+        public int Age(string age)
+        {
+            if (Regex.IsMatch(age, @"^[0-9]{1,3}$"))
+            {
+                _Age = Int32.Parse(age);
+            }
+
+            return _Age;
+        }
+
+        public FrmRegistration()
+        {
+            InitializeComponent();
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmRegistration_Load(object sender, EventArgs e)
+        {
+            string[] ListOfProgram = new string[]
+            {
+            "BS Information Technology",
+            "BS Computer Science",
+            "BS Information Systems",
+            "BS in Accountancy",
+            "BS in Hospitality Management",
+            "BS in Tourism Management"
+            };
+            for (int i = 0; i < 6; i++)
+            {
+                cbPrograms.Items.Add(ListOfProgram[i].ToString());
+            }
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            StudentInformationClass.SetFullName = FullName(txtLastName.Text, txtFirstName.Text, txtMiddleInitial.Text);
+            StudentInformationClass.SetStudentNo = StudentNumber(txtStudentNo.Text);
+            StudentInformationClass.SetProgram = cbPrograms.Text;
+            StudentInformationClass.SetGender = cbGender.Text;
+            StudentInformationClass.SetStudentContactNo = ContactNo(txtContactNo.Text);
+            StudentInformationClass.SetAge = Age(txtAge.Text);
+            StudentInformationClass.SetBirthday = datePickerBirthday.Value.ToString("yyyy-MM-dd");
+            FrmConfirmation frm = new FrmConfirmation();
+            frm.ShowDialog();
+        }
+
+        private void cbPrograms_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtStudentContactNo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
